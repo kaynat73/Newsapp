e1443eb2d969538c0a2177cf9196d05fb191f80c
kaynat73 <135855191+kaynat73@users.noreply.github.com>
2023-09-23 02:35:43 -0700

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import NewsItems from "./NewsItems";
+import Spinner from "./spinner";
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const [page, setPage] = useState(1);
+
+  const [totalResults, setTotalResult] = useState(0);
+
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=128404a9d68849ca8d2910a9e308816f&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
+    let data = await fetch(url);
+    props.setProgress(30);
+
+    let parsedData = await data.json();
+    props.setProgress(70);
+    setArticles(parsedData.articles);
+    setTotalResult(parsedData.totalResults);
+    setLoading(false);
+
+    props.setProgress(100);
+  };
+  useEffect(() => {
+    updateNews(); 
+    //eslint-disable-next-line 
+  }, [])
+
+
+const fetchMoreData = async () => {
+  setPage(page + 1);
+  const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=128404a9d68849ca8d2910a9e308816f&page=${page+1}&pageSize=${props.pageSize}`;
+
+  let data = await fetch(url);
+  let parsedData = await data.json();
+  setArticles(articles.concat(parsedData.articles));
+  setTotalResult(parsedData.totalResults);
+};
+
+return(
+  <>
+    <h1 className="text-center" style={{ margin: '35px 0px' ,marginTop:'90px' }}>The Muslim - Top Headlines
+    </h1>
+    {loading && <Spinner />}
+    <InfiniteScroll
+      dataLength={articles.length}
+      next={fetchMoreData}
+      hasMore={articles.length !== totalResults}
+      loader={<Spinner />}
+    >
+      <div className="container">
+        <div className="row">
+          {articles.map((element) => {
+            return <div className="col-md-4" key={element.url}>
+                <NewsItems title = {element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
+                />
+              </div>
+            
+          })}
+        </div>
+      </div>
+    </InfiniteScroll>
+  </>
+);
+
+}
+News.defaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
+News.PropsTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
+export default News;
